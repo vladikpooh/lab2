a2ac96fe93c5130026c2fc4d56610c37c4963b47
lagvlad <lagvlad96@gmail.com>
2017-04-17 23:14:51 +0300

    Weather parser

---
+++ minshweather.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+city=$(curl http://pogoda.tut.by/city/minsk -silent | grep '<a href="" class="dotted" data-weathercode="26850">' | sed -e's/<\/h1>//g' |  sed -e's/<h1>//g' | sed -e's/<\/a>//g' | sed -e's/<a href="" class="dotted" data-weathercode="26850">//g' | sed -e's/<div class="b-modes m-media">//g' | sed -e's/<ul class="b-modes-list">//g' | sed -e's/<li class="b-mode__li">//g')
+echo $city
+temp=$(curl http://pogoda.tut.by/city/minsk -silent | sed -nre '/h1/,/fcurrent-descr/p' | grep 'temp-i' | sed -e's/<span class="temp-i">//g' | sed -e's/<\/div>//g' | sed -e's/<\/span>//g' | sed -e's/&deg;//g')
+echo $temp
